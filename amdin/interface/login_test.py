94feb443ed4e85f43f253087896d54c927a0d5c4
Demi <zhangmengdi929@163.com>
2019-07-12 21:53:00 +0800

    登陆用例增加

--- amdin/interface/login_test.py
+++ amdin/interface/login_test.py
@@ -47,6 +47,46 @@ class loginTest(unittest.TestCase):
 		time.sleep(3)
 		now_url=self.driver.current_url
 		self.assertEqual(now_url,"http://admin.muggle-inc.com/desktop")
+	def test_login3(self):
+		'''用户名&密码均为空'''
+		#读取txt文件
+		username=''
+		password=''
+		
+		self.driver.find_element_by_name('username').send_keys(username)
+		self.driver.find_element_by_name('password').send_keys(password)
+#		time.sleep(3)
+		self.driver.find_element_by_tag_name('button').click()
+		time.sleep(3)
+		now_url=self.driver.current_url
+		self.assertNotEqual(now_url,"http://admin.muggle-inc.com/desktop")
+	def test_login4(self):
+		'''用户名/密码均为空'''
+		#读取txt文件
+		username='test'
+		password=''
+		
+		self.driver.find_element_by_name('username').send_keys(username)
+		self.driver.find_element_by_name('password').send_keys(password)
+#		time.sleep(3)
+		self.driver.find_element_by_tag_name('button').click()
+		time.sleep(3)
+		now_url=self.driver.current_url
+		self.assertNotEqual(now_url,"http://admin.muggle-inc.com/desktop")
+	def test_login5(self):
+		'''密码错误'''
+		#读取txt文件
+		username='test'
+		password='qq12345678'
+		
+		self.driver.find_element_by_name('username').send_keys(username)
+		self.driver.find_element_by_name('password').send_keys(password)
+#		time.sleep(3)
+		self.driver.find_element_by_tag_name('button').click()
+		time.sleep(3)
+		now_url=self.driver.current_url
+		self.assertNotEqual(now_url,"http://admin.muggle-inc.com/desktop")
+
 
 '''	def test_login(self):
 		
